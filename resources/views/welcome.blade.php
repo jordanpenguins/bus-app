33bf90b62d1b7b522f1364588c2aa53eef4ed1fe
jordanpenguins <136601885+jordanpenguins@users.noreply.github.com>
2024-11-03 22:40:26 +1100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<x-front-layout>
+    <div class="flex justify-between items-center min-h-screen ">
+        <div>
+            <h1 class="text-8xl">hello world...</h1>
+            <p class="ml-1 mt-5">Express bus between Malaysia and Singapore</p>
+        </div>
+        <div class="hidden lg:block pr-4 md:pr-12">
+            <img src= "{{ asset('home.jpg') }}" alt="bus" class="w-96 h-96 object-cover rounded-lg shadow-lg">
+        </div>
+    </div>
+    <!-- description section -->
+    <div class="mt-8 py-8 mb-8 p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md flex justify-between">
+        <div>
+            <h2 class="text-2xl font-bold text-gray-900 dark:text-white">Why Choose Us?</h2>
+        </div>
+        <div>
+            <p class="text-gray-700 dark:text-gray-300">
+                This is the description of the web page. It provides an overview of the content and purpose of the page.
+            </p>
+            <p class="text-gray-700 dark:text-gray-300">
+                This is the description of the web page. It provides an overview of the content and purpose of the page.
+            </p>
+            <p class="text-gray-700 dark:text-gray-300">
+                This is the description of the web page. It provides an overview of the content and purpose of the page.
+            </p>
+        </div>
+
+
+    </div>
+
+
+</x-front-layout>
